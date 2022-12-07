0b2fcffb2811b375ea05637f93940e895b91afcd
GumoDev <116546004+GumoDev@users.noreply.github.com>
2022-12-06 22:16:19 -0600

    Update app.js

--- app.js
+++ app.js
@@ -21,37 +21,8 @@ connection.connect((err)=>{
         console.log("DB Connected");
     }
 })
-app.get('/api', (req, res) => {
-    connection.query("SELECT * FROM users" , (err, rows)=>{
-        console.log(rows)
-        res.send(rows)
-    })
-})
-app.post('/api', (req, res )=>{
-    connection.query("INSERT INTO users (id, Name) values (null, 'Prueba ')"),(err, rows)=>{
-        res.send(rows)
-    }
-})
-app.patch('/api/:id', (req, res)=>{
-    connection.query(`UPDATE users SET Name = 'Prueba patching 5' WHERE users.id = ${req.params.id}`, (err, rows)=>{
-        res.send(
-            req.params.id,
-            rows
-        )
-    })
-})
-app.delete('/api/:id', (req, res)=>{
-    connection.query(`DELETE FROM users WHERE users.id = ${req.params.id}`, (err, rows)=>{
-        res.send(
-            
-            rows
-        )
-    })
-})
-
-app.get("/index.html", (req, res)=>{
-    app.use(express.static(__filename + 'index.html'))
-    
+app.get("/", (req, res)=>{
+    res.send("Hello API")
 })
 
 

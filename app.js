2286ffa1bfedba7ba10c60a7eb152da89c7f0d7f
GumoDev <116546004+GumoDev@users.noreply.github.com>
2022-12-06 22:22:17 -0600

    Update app.js

--- app.js
+++ app.js
@@ -8,19 +8,6 @@ app.listen(port || 0, ()=>{
     console.log("api works server listenning at localhost: " + port);
 })
 
-const connection = mysql.createConnection({
-    host: "dpg-ce7p3nh4rebbibj17np0-a",
-    database: "node_h04s",
-    user: "root",
-    password: "rhiP5jSyrju9b5ZE70tv9VyumyJL7kPT",
-})
-connection.connect((err)=>{
-    if(err){
-        throw err
-    } else {
-        console.log("DB Connected");
-    }
-})
 app.get("/", (req, res)=>{
     res.send("Hello API")
 })

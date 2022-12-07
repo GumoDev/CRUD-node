61583f085c11673b8e899305643a11268c0b5213
GumoDev <116546004+GumoDev@users.noreply.github.com>
2022-12-06 23:22:18 -0600

    Update app.js

--- app.js
+++ app.js
@@ -4,21 +4,6 @@ const mysql = require("mysql")
 const app = express();
 const port = process.env.PORT || 3001;
 
-const connection = mysql.createConnection({
-    host: process.env.HOST,
-    database: process.env.DATABASE,
-    user: process.env.USER,
-    password: process.env.PASSWORD
-})
-
-connection.connect((err)=>{
-    if(err){
-        throw err
-    } else {
-        console.log("DB Connected");
-    }
-})
-
 app.get("/", (req, res) => res.send("Hello API Node Js & Express"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));

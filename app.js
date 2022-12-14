26b1a796ce5d3ec3096afafb66020f2a973e20b5
Gustavo Park <80788755+StzaryC@users.noreply.github.com>
2022-12-14 13:38:53 -0600

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const mysql = require("mysql")
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.send("Hello API Node Js & Express"));
+app.get("/", (req, res) => res.send(`Hello API Node Js & Express ${port}`));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
